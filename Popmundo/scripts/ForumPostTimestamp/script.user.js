050262463f5c40df6710ad2618feb835e1f00c62
Ahmet <criyessei@gmail.com>
2023-05-29 17:10:54 +0300

    Added user script named ForumPostTimestamp

---
+++ Popmundo/scripts/ForumPostTimestamp/script.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Forum Post Timestamp
+// @description  The script run only on forms pages and show timestamps of messages
+
+// @author       Ahmet Emsal
+// @version      1.0.0
+
+// @include      /^https?://\d+.popmundo.com/Forum/Popmundo.aspx/Thread/*/
+
+// @grant        unsafeWindow
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js
+
+
+// @require      https://raw.githubusercontent.com/AhmetEmsal/user-scripts/master/Popmundo/scripts/ForumPostTimestamp/dist/script.min.js
+// @run-at       document-end
+
+// ==/UserScript==
+console.log("Run!");
